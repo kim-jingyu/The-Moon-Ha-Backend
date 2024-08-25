5608e74971365a3773b21cac1a86911523378c3a
yookyung Choi <ykc131719@gmail.com>
2024-08-25 20:13:47 +0900

    [KAN-11] Feat: PasswordEncoderConfig 생성

---
+++ src/main/java/com/innerpeace/themoonha/global/config/PasswordEncoderConfig.java
@@ -0,0 +1,26 @@
+package com.innerpeace.themoonha.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * PasswordEncoder 설정
+ * @author 최유경
+ * @since 2024.08.25
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.25  	최유경       최초 생성
+ * </pre>
+ */
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
