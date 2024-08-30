f6a4a3b701cabedddcd0ef95c95207c6e2faa9b9
Hypersand <on8214@gmail.com>
2024-08-30 10:22:57 +0900

    [KAN-12] Feat: 강좌 목록 조회 쿼리 및 로직 수정

--- src/main/java/com/innerpeace/themoonha/domain/lesson/dto/LessonListResponse.java
+++ src/main/java/com/innerpeace/themoonha/domain/lesson/dto/LessonListResponse.java
@@ -14,10 +14,10 @@ import java.util.List;
  * 수정일        수정자        수정내용
  * ----------  --------    ---------------------------
  * 2024.08.24  	손승완       최초 생성
+ * 2024.08.30  	손승완       정적 팩토리 메서드 패턴 추가 및 setter 제거
  * </pre>
  */
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -26,4 +26,15 @@ public class LessonListResponse {
     private List<ShortFormDTO> shortFormList;
     private String memberName;
     private List<LessonDTO> lessonList;
+
+    public static LessonListResponse of(LessonListDTO lessonList, List<ShortFormDTO> shortFormList,
+                                        String memberName) {
+
+        return LessonListResponse.builder()
+                .lessonList(lessonList.getLessonList())
+                .branchName(lessonList.getBranchName())
+                .shortFormList(shortFormList)
+                .memberName(memberName)
+                .build();
+    }
 }
