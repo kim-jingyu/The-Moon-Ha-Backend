2aff427d85ca9014a9a705985d0503142cdfa2d0
yookyung Choi <ykc131719@gmail.com>
2024-09-10 15:34:13 +0900

    [KAN-60] Feat: health-check 추가, origin 도메인 추가

--- src/main/java/com/innerpeace/themoonha/global/config/CORSFilter.java
+++ src/main/java/com/innerpeace/themoonha/global/config/CORSFilter.java
@@ -1,12 +1,14 @@
 package com.innerpeace.themoonha.global.config;
 
 import java.io.IOException;
+import java.util.Arrays;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,8 +29,22 @@ public class CORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        // 허용할 출처 목록
+        String[] allowedOrigins = {"http://localhost:3000", "http://web.themoonha.site"};
+        String origin = request.getHeader("Origin");
+
+        // 요청 출처가 허용된 출처 목록에 포함되어 있는지 확인
+        boolean isAllowed = Arrays.asList(allowedOrigins).contains(origin);
+
+        if (isAllowed) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        } else {
+            response.setHeader("Access-Control-Allow-Origin", ""); // 허용되지 않은 출처의 경우 빈 문자열로 설정
+        }
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept, Authorization");
