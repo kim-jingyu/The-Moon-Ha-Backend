d04673cf3706c1137cf6af5d0c1664e4412e5d4b
yookyung Choi <ykc131719@gmail.com>
2024-09-13 08:31:50 +0900

    [KAN-11] Feat: 로그아웃 구현

--- src/main/java/com/innerpeace/themoonha/domain/auth/util/AuthUtil.java
+++ src/main/java/com/innerpeace/themoonha/domain/auth/util/AuthUtil.java
@@ -12,6 +12,7 @@ import javax.servlet.http.Cookie;
  * 수정일        수정자        수정내용
  * ----------  --------    ---------------------------
  * 2024.08.27  	최유경       최초 생성
+ * 2024.09.13   최유경       쿠키 삭제 구현
  * </pre>
  */
 public class AuthUtil {
@@ -31,6 +32,16 @@ public class AuthUtil {
         cookie.setSecure(false); // cookie.setSecure(true); HTTPS 를 사용하는 경우만 쿠키를 전송하도록 설정
         cookie.setPath(COOKIE_PATH); // 쿠키의 유효 경로 설정
         cookie.setMaxAge(COOKIE_MAX_AGE); // 쿠키 유효 기간 설정
+        return cookie;
+    }
+
+    public static Cookie createJwtTokenDeleteCookie(String key) {
+        Cookie cookie = new Cookie(key, "");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath(COOKIE_PATH);
+        cookie.setMaxAge(0);
+
         return cookie;
     }
 }
